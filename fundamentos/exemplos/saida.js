d5a0fb8698008791b0e594ef3f9bba4c80cb6bca
Henrique Jacomini <henrique.jacomini@aluno.senai.br>
2026-02-06 09:50:51 -0300

    arquivos saida

---
+++ fundamentos/exemplos/saida.js
@@ -0,0 +1,8 @@
+const nome = prompt ("Qual seu primeiro nome?")
+const endereco = prompt("Qual seu endereço?")
+const idade = prompt("Qual sua idade?")
+
+alert(`
+    ${nome}, vc tem ${idade} anos, \n mora no 
+    endereco ${endereco}
+    `)
