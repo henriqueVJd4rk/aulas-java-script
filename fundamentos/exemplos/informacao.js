dd29f9d5d20d568ee369a7c15162bb35bb2f591f
Henrique Jacomini <henrique.jacomini@aluno.senai.br>
2026-02-06 09:22:05 -0300

    arquivo informacao

---
+++ fundamentos/exemplos/informacao.js
@@ -0,0 +1,12 @@
+const curso ="Técnico em Desenvolvimente de Sistemas"
+const ano = 2025
+const notaMinimaParaSerAprovado = 50
+const notaParaReprovado = 49.9
+const cursoTecnico = true
+
+console.log('Minhas informações acadêmicas')
+console.log("Curso:" + curso)
+console.log(`Ano: ${ano}`)
+console.log(`Nota para aprovação ${notaMinimaParaSerAprovado}`)
+console.log(`Nota mínima para ser reprovado ${notaParaReprovado}`)
+console.log(`É curso técnico: ${cursoTecnico}`)
